1000b273b6e259426d0cf634338a2802ea1ce76d
Račinský Matěj <matej.racinsky@avast.com>
2020-02-14 10:15:50 +0100

    fixing problems with precompilations

--- src/utils.jl
+++ src/utils.jl
@@ -3,7 +3,7 @@
 
 	dictionary with labels to indexes
 """
-Zygote.@nograd function labelmap(y::AbstractVector{T}) where {T}
+function labelmap(y::AbstractVector{T}) where {T}
 	d = Dict{T, Vector{Int}}()
 	for (i,v) in enumerate(y)
 		if haskey(d, v)
@@ -15,13 +15,15 @@ Zygote.@nograd function labelmap(y::AbstractVector{T}) where {T}
 	d
 end
 
+Zygote.@nograd labelmap
+
 """
 	clusters, iclusters = identifyclusters(y)
 
 	create a vector of indexes of samples with the same label
 	and its inverse mapping
 """
-Zygote.@nograd function identifyclusters(y)
+function identifyclusters(y)
 	yc = labelmap(y)
 	clusters = [yc[k] for k in keys(yc)]
 	iclusters = Vector{Int}(undef, length(y))
@@ -32,3 +34,5 @@ Zygote.@nograd function identifyclusters(y)
 	end
 	return(clusters, iclusters)
 end
+
+Zygote.@nograd identifyclusters
